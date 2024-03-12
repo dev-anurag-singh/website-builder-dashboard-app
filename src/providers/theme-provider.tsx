161ae4119ed87c6310b4ff5874f4fef251fa2d48
Anurag <dev.anuragkr@gmail.com>
2024-03-12 23:16:46 +0530

    added theme provider

---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { ThemeProviderProps } from 'next-themes/dist/types';
+
+function Provider({ children, ...props }: ThemeProviderProps) {
+  return <ThemeProvider {...props}>{children}</ThemeProvider>;
+}
+
+export default Provider;
