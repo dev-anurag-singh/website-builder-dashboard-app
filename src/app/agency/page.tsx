817cc118994fac65107b5afebeb7058224c15047
Anurag <dev.anuragkr@gmail.com>
2024-03-13 23:35:38 +0530

    agency page

--- src/app/agency/page.tsx
+++ src/app/agency/page.tsx
@@ -1,5 +1,48 @@
-function Page() {
-  return <div>Agency Page</div>;
+import { verifyAndAcceptInvitation } from '@/actions/invitation';
+import { getAuthUserDetails } from '@/actions/user';
+import { currentUser } from '@clerk/nextjs';
+import { Plan } from '@prisma/client';
+import { redirect } from 'next/navigation';
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { state: string; code: string };
+}) {
+  let user = await getAuthUserDetails();
+
+  if (!user) {
+    await verifyAndAcceptInvitation();
+    user = await getAuthUserDetails();
+  }
+
+  if (user) {
+    // USER IS PRESENT
+    if (user.role === 'AGENCY_OWNER' || user.role === 'AGENCY_ADMIN') {
+      if (searchParams.state) {
+        const statePath = searchParams.state.split('___')[0];
+        const stateAgencyId = searchParams.state.split('___')[1];
+        if (!stateAgencyId) return <div>Not authorized</div>;
+        return redirect(
+          `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`
+        );
+      }
+      return redirect(`/agency/${user.agencyId}`);
+    }
+    // USER HAS SUBACCOUNT
+    return redirect('/subaccount');
+  }
+
+  // Visitor is not a agency user or has an invitation
+  const authUser = await currentUser();
+
+  return (
+    <div className='flex justify-center items-center mt-4'>
+      <div className='max-w-[850px] border p-4 rounded-xl'>
+        <h1 className='text-4xl'>Create an agency</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
