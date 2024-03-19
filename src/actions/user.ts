c3d780c610c8e3aadcf6c43dc24b4155fe548e51
Anurag <dev.anuragkr@gmail.com>
2024-03-19 22:06:04 +0530

    added init user server action

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { db } from '@/lib/db';
-import { currentUser } from '@clerk/nextjs';
+import { clerkClient, currentUser } from '@clerk/nextjs';
 import { User } from '@prisma/client';
 
 export const getAuthUserDetails = async () => {
@@ -36,4 +36,32 @@ export const createUser = async (user: User) => {
   if (user.role === 'AGENCY_OWNER') return null;
   const response = await db.user.create({ data: user });
   return response;
+};
+
+export const initUser = async (newUser: Partial<User>) => {
+  const user = await currentUser();
+
+  if (!user) return;
+
+  const userData = await db.user.upsert({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+    },
+    update: newUser,
+    create: {
+      id: user.id,
+      avatarUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+      name: `${user.firstName} ${user.lastName}`,
+      role: newUser.role || 'SUBACCOUNT_USER',
+    },
+  });
+
+  await clerkClient.users.updateUserMetadata(user.id, {
+    privateMetadata: {
+      role: newUser.role || 'SUBACCOUNT_USER',
+    },
+  });
+
+  return userData;
 };
